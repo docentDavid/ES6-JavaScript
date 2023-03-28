17230db70aade66989fe25f48f62d73d9a5e0ac0
David Schol <d.schol@fontys.nl>
2023-03-28 10:34:43 +0200

    map added

---
+++ src/7.map.js
@@ -0,0 +1,33 @@
+import "./index.css";
+
+let appNode = document.getElementById("app");
+
+// const getallen = [1, 4, 9, 16];
+
+// const getal = getallen.map((getal) => {
+//   return getal ** 2; // Kwadraat
+// });
+
+const personen = [
+  {
+    voornaam: "Marthe",
+    leeftijd: 19,
+    isGetrouwd: false,
+    studeertNog: true
+  },
+  {
+    voornaam: "Peter",
+    leeftijd: 25,
+    isGetrouwd: true,
+    studeertNog: true
+  }
+];
+
+const persoon = personen.map((persoon) => {
+  return `
+  <p>${persoon.voornaam}</p>
+  <p>${persoon.leeftijd}</p>
+  `;
+});
+
+appNode.innerHTML = persoon;
