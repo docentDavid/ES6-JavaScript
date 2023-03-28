b9d0e7055f29b39ff0932211ee1670f7c72f657e
David Schol <d.schol@fontys.nl>
2023-03-28 10:36:32 +0200

    Updates added

--- src/7.map.js
+++ src/7.map.js
@@ -2,32 +2,32 @@ import "./index.css";
 
 let appNode = document.getElementById("app");
 
-// const getallen = [1, 4, 9, 16];
+const getallen = [1, 4, 9, 16];
 
-// const getal = getallen.map((getal) => {
-//   return getal ** 2; // Kwadraat
-// });
-
-const personen = [
-  {
-    voornaam: "Marthe",
-    leeftijd: 19,
-    isGetrouwd: false,
-    studeertNog: true
-  },
-  {
-    voornaam: "Peter",
-    leeftijd: 25,
-    isGetrouwd: true,
-    studeertNog: true
-  }
-];
-
-const persoon = personen.map((persoon) => {
-  return `
-  <p>${persoon.voornaam}</p>
-  <p>${persoon.leeftijd}</p>
-  `;
+const getal = getallen.map((getal) => {
+  return getal ** 2; // Kwadraat
 });
 
-appNode.innerHTML = persoon;
+// const personen = [
+//   {
+//     voornaam: "Marthe",
+//     leeftijd: 19,
+//     isGetrouwd: false,
+//     studeertNog: true
+//   },
+//   {
+//     voornaam: "Peter",
+//     leeftijd: 25,
+//     isGetrouwd: true,
+//     studeertNog: true
+//   }
+// ];
+
+// const persoon = personen.map((persoon) => {
+//   return `
+//   <p>${persoon.voornaam}</p>
+//   <p>${persoon.leeftijd}</p>
+//   `;
+// });
+
+appNode.innerHTML = getal;
